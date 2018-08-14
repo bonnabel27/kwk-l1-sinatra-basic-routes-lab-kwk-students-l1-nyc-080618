c4532926b91918bdc219730a46f926b9eaf29472
bonnabel27 <sika.attikesse@gmail.com>
2018-08-14 15:37:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,10 +7,11 @@ class App < Sinatra::Base
 
 get '/name' do 
   'My name is Sika!'
-  
+end
   get '/home'
   'I am from NY'
-  
+end
  get '/music'
  'My favorite song is Top Striker'
-end
+ end
+end 
